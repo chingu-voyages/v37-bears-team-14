1938a37038525d3449b8dffcc49750b3bc420721
seann1 <seanniesen@gmail.com>
2022-02-09 11:15:28 -0800

    add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Hello from "./Hello";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="flex">
-      <div className="border-2 border-stone-500 p-8 m-12 rounded-md">
-        <Hello />
+    <>
+      <Navbar
+        username="Bill"
+        avatarURL="https://s3-us-east-2.amazonaws.com/maryville/wp-content/uploads/2020/01/20133422/software-developer-coding-500x333.jpg"
+      />
+      <div className="flex">
+        <div className="border-2 border-stone-500 p-8 m-12 rounded-md">
+          <Hello />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/Navbar.tsx
@@ -0,0 +1,53 @@
+import React, { FunctionComponent } from "react";
+type NavbarProps = {
+  username: string;
+  avatarURL: string;
+};
+const Navbar: FunctionComponent<NavbarProps> = ({ username, avatarURL }) => {
+  return (
+    <>
+      <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+        <div className="mb-2 sm:mb-0">
+          <a
+            href="/home"
+            className="text-2xl no-underline text-grey-darkest hover:text-blue-dark"
+          >
+            Link Up
+          </a>
+        </div>
+        <div>
+          <span className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">
+            {username}
+          </span>
+          <img src={avatarURL} alt="" className="h-10 inline rounded" />
+          <a
+            href="/projects"
+            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+          >
+            Projects
+          </a>
+          <a
+            href="/developers"
+            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+          >
+            Developers
+          </a>
+          <a
+            href="/applications"
+            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+          >
+            Applications
+          </a>
+          <a
+            href="/sign-in"
+            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+          >
+            Sign In
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
