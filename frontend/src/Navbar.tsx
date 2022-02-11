0ca0ef5d65881a40522d44a710358167c6fe38c5
seann1 <seanniesen@gmail.com>
2022-02-11 00:50:44 -0800

    fix errors in Navbar.tsx

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -84,19 +84,19 @@ const Navbar: FunctionComponent = () => {
               <img
                 className="rounded-full border border-gray-100 shadow-sm w-16 h-16 inline cursor-pointer border-transparent border-4 transition duration-900 hover:border-gray-300 "
                 src={currentSession.user.avatarUrl}
-                alt="user image"
+                alt="avatar"
                 onClick={() => setProfileDiv(!profileDiv)}
               />
               {profileDiv && (
                 <div className="absolute right-0 mt-2 w-48 bg-white rounded-md overflow-hidden shadow-xl z-20 mr-2">
                   <a
-                    href="#"
+                    href="/signout"
                     className="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"
                   >
                     Sign Out
                   </a>
                   <a
-                    href="#"
+                    href="/userprofile"
                     className="block px-4 py-2 text-sm text-gray-800 border-b hover:bg-gray-200"
                   >
                     View Profile
