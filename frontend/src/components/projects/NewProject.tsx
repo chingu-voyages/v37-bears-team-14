f1cbde33ee5d87226d8b1ca331d37760c39861ba
seann1 <seanniesen@gmail.com>
2022-02-15 21:45:19 -0800

    removing flowbite, using headlessui instead

--- frontend/src/components/projects/NewProject.tsx
+++ frontend/src/components/projects/NewProject.tsx
@@ -73,49 +73,6 @@ const NewProject: FunctionComponent = () => {
                   <button type="button" className="orange-btn">
                     Add Technology
                   </button>
-
-                  <div className="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4">
-                    <div className="px-4 py-3">
-                      <span className="block text-sm">Bonnie Green</span>
-                      <span className="block text-sm font-medium text-gray-900 truncate">
-                        name@flowbite.com
-                      </span>
-                    </div>
-                    <ul className="py-1" aria-labelledby="dropdown">
-                      <li>
-                        <a
-                          href="#"
-                          className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-                        >
-                          Dashboard
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
-                          className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-                        >
-                          Settings
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
-                          className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-                        >
-                          Earnings
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
-                          className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-                        >
-                          Sign out
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
                 </div>
                 <div className="flex items-center justify-between">
                   <button type="submit" className="main-btn">
