47f631b920fc66868a202cc8143db7945f77cec3
seann1 <seanniesen@gmail.com>
2022-02-15 23:59:08 -0800

    add techs dropdown and tech selection

--- frontend/src/components/projects/NewProject.tsx
+++ frontend/src/components/projects/NewProject.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
+import { Menu } from "@headlessui/react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
@@ -8,6 +9,32 @@ const NewProjectSchema = Yup.object().shape({
 });
 
 const NewProject: FunctionComponent = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [techs, setTechs] = useState<any[]>([]);
+  const [chosenTechs, setChosenTechs] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch("/api/v1/techs").then(async (response) => {
+      if (response.status === 200) {
+        const data = await response.json();
+
+        setTechs(data);
+      } else {
+        console.error(
+          "failed to load techs",
+          response.status,
+          await response.json()
+        );
+      }
+    });
+  }, []);
+
+  const chooseTech = (e: any, chosenTech: object) => {
+    const updatedTechs = techs.filter((tech) => tech !== chosenTech);
+    setTechs(updatedTechs);
+    setChosenTechs([...chosenTechs, chosenTech]);
+  };
+
   return (
     <>
       <div className="container mx-auto px-4">
@@ -19,6 +46,8 @@ const NewProject: FunctionComponent = () => {
             }}
             validationSchema={NewProjectSchema}
             onSubmit={(values) => {
+              // same shape as initial values
+              console.log(values);
               async function postProject(values: object) {
                 const response = await fetch("api/v1/projects", {
                   method: "POST",
@@ -27,11 +56,9 @@ const NewProject: FunctionComponent = () => {
                   },
                   body: JSON.stringify(values),
                 });
-                const data = await response.json();
+                // const data = await response.json();
               }
               postProject(values);
-              // same shape as initial values
-              console.log(values);
             }}
           >
             {({ errors, touched }) => (
@@ -68,15 +95,65 @@ const NewProject: FunctionComponent = () => {
                 {errors.description && touched.description ? (
                   <div>{errors.description}</div>
                 ) : null}
-
-                <div className="flex items-center justify-between">
-                  <button type="button" className="orange-btn">
+                <div className="flex justify-start">
+                  {chosenTechs.map((tech) => (
+                    <img className="h-12 w-12 m-2" src={tech.imageUrl} />
+                  ))}
+                </div>
+                <div className="mt-2">
+                  {/* <button type="button" className="orange-btn">
                     Add Technology
-                  </button>
+                  </button> */}
+                  <Menu>
+                    <Menu.Button className="orange-btn">
+                      Add Technologies
+                    </Menu.Button>
+                    <Menu.Items className="bg-white max-w-lg text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4 max-h-[300px] overflow-y-scroll">
+                      {techs
+                        .sort((a, b) => (a.name > b.name ? 1 : -1))
+                        .map((tech, index) => (
+                          <Menu.Item key={index}>
+                            {({ active }) => (
+                              <>
+                                <div
+                                  onClick={(e) => chooseTech(e, tech)}
+                                  className="flex justify-between items-center h-12 p-2 cursor-pointer hover:bg-neutral-300 align-baseline"
+                                >
+                                  <img
+                                    className=" h-10"
+                                    src={tech?.imageUrl}
+                                    alt={tech.name}
+                                  />
+
+                                  <a
+                                    className={`block p-2 ${
+                                      active && "bg-blue-500"
+                                    }`}
+                                    href="/account-settings"
+                                  >
+                                    {tech.name}
+                                  </a>
+                                </div>
+                              </>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      {/* <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            className={`block p-2 ${active && "bg-blue-500"}`}
+                            href="/account-settings"
+                          >
+                            Documentation
+                          </a>
+                        )}
+                      </Menu.Item> */}
+                    </Menu.Items>
+                  </Menu>
                 </div>
                 <div className="flex items-center justify-between">
                   <button type="submit" className="main-btn">
-                    Submit
+                    Create Project
                   </button>
                 </div>
               </Form>
