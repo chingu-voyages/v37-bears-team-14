2dfb345965c7755832b849dc3d08ea9c5119d351
Matthew <matthewt@wepay.com>
2022-02-21 08:08:15 -0800

    Implement session hook

---
+++ frontend/src/hooks/session.tsx
@@ -0,0 +1,79 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export type SessionData = {
+  loading: boolean;
+  isLoggedIn: boolean;
+  user: null | {
+    avatarUrl: string;
+    createdAt: string;
+    githubId: string;
+    id: string;
+    isAdmin: boolean;
+    updatedAt: string;
+    username: string;
+  };
+};
+
+const initialData: SessionData = {
+  loading: true,
+  isLoggedIn: false,
+  user: null,
+};
+
+export const SessionContext = createContext(initialData);
+
+export function SessionProvider(props: any) {
+  const [session, setSession] = useState(initialData);
+
+  useEffect(() => {
+    if (!session.loading) {
+      return;
+    }
+
+    const fetchSession = async () => {
+      const getSession = async () => {
+        const resp = await fetch("/api/v1/current-session");
+        if (resp.status === 200) {
+          return await resp.json();
+        }
+        if (resp.status >= 400) {
+          console.error("Session fetch error", resp);
+        }
+        return { isLoggedIn: false, user: null };
+      };
+
+      try {
+        const session = await getSession();
+        setSession({
+          loading: false,
+          ...session,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchSession();
+  }, [session]);
+
+  return (
+    <SessionContext.Provider value={session}>
+      {props.children}
+    </SessionContext.Provider>
+  );
+}
+
+/**
+ * Usage:
+ *     function Component() {
+ *         const { loading, isLoggedIn, user } = useSession();
+ *         return loading || !isLoggedIn ? (
+ *             <div>Not Logged In</div>
+ *         ) : (<div>{user.username} logged in</div>)
+ *     }
+ *
+ * @returns () => SessionData
+ */
+export function useSession() {
+  return useContext(SessionContext);
+}
