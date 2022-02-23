cf6c076c3077fa3b1e9575ab1a69a25293c70938
seann1 <seanniesen@gmail.com>
2022-02-23 13:50:54 -0800

    add link to project preview

--- frontend/src/components/projects/ProjectPreview.tsx
+++ frontend/src/components/projects/ProjectPreview.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 import { Project, Tech } from "../../shared/Interfaces";
 
 interface Props {
@@ -10,30 +11,32 @@ const ProjectPreview: FunctionComponent<Props> = ({ projects }) => {
     // <div className="md:container mx-auto px-4"></div>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 m-1">
       {projects.map((p, index) => (
-        <div className="bg-neutral-400" key={index}>
-          <div className="bg-darkGray border-b-4 border-lightGray">
-            <p className="text-center sm:text-md lg:text-xl text-lightGray pb-2 pt-1">
-              {p.name}
-            </p>
-          </div>
-          <div className="pt-1">
-            <p className="mb-2 text-md text-darkGray pl-2">{p.description}</p>
-            <div className="flex justify-start">
-              {p.techs.map((t: Tech, index) => (
-                <div className="relative m-1" key={index}>
-                  <img
-                    className="h-9 mx-auto flex justify-center"
-                    alt={t.name}
-                    src={t.imageUrl}
-                  />
-                  <div className="p-1 m-1 text-xs text-slate-600 bg-slate-300 rounded-md flex justify-center font-bold">
-                    {t.name}
+        <Link to={p.id}>
+          <div className="bg-neutral-400" key={index}>
+            <div className="bg-darkGray border-b-4 border-lightGray">
+              <p className="text-center sm:text-md lg:text-xl text-lightGray pb-2 pt-1">
+                {p.name}
+              </p>
+            </div>
+            <div className="pt-1">
+              <p className="mb-2 text-md text-darkGray pl-2">{p.description}</p>
+              <div className="flex justify-start">
+                {p.techs.map((t: Tech, index) => (
+                  <div className="relative m-1" key={index}>
+                    <img
+                      className="h-9 mx-auto flex justify-center"
+                      alt={t.name}
+                      src={t.imageUrl}
+                    />
+                    <div className="p-1 m-1 text-xs text-slate-600 bg-slate-300 rounded-md flex justify-center font-bold">
+                      {t.name}
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
