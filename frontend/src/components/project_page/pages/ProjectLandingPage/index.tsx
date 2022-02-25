b2cdf1484de6856618a7d03d3c128918341a4f44
mtso <~@mtso.io>
2022-02-24 23:19:45 -0800

    Add tech stack update modal to project page

--- frontend/src/components/project_page/pages/ProjectLandingPage/index.tsx
+++ frontend/src/components/project_page/pages/ProjectLandingPage/index.tsx
@@ -1,14 +1,14 @@
 import { useOutletContext } from "react-router-dom";
-import { Project } from "../../../../shared/Interfaces";
-import ProjectLanding from "./ProjectLanding";
+import { ProjectPageContext } from "../../layouts/ProjectPageLayout";
+import ProjectLanding from "./components/ProjectShowcase";
 
 const ProjectLandingPage = () => {
-  const { project } = useOutletContext<{ project: Project }>();
+  const { project, setProject } = useOutletContext<ProjectPageContext>();
 
   return (
     <div className="flex flex-col-reverse md:flex-row">
       <main className="basis-3/4">
-        <ProjectLanding project={project} />
+        <ProjectLanding project={project} setProject={setProject} />
       </main>
       <aside className="basis-1/4"></aside>
     </div>
