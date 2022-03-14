4a7dbff662f180db7996259ffdce14ecf89e12c6
seann1 <seanniesen@gmail.com>
2022-03-14 09:36:46 -0700

    separate star button out into its own component

--- frontend/src/components/project_page/pages/ProjectLandingPage/components/StarButton.tsx
+++ frontend/src/components/project_page/pages/ProjectLandingPage/components/StarButton.tsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { FC } from "react";
 import ActionButton from "../../../../controls/ActionButton";
 import { useSession } from "../../../../../hooks/session";
 import { useParams } from "react-router-dom";
-const { loading, isLoggedIn, user } = useSession();
-const params = useParams();
-const starProject = () => {
-  fetch(`/api/v1/projects/${params.projectId}/star`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ user: user, project: params.projectId }),
-  });
-  console.log(user);
+import { Project } from "../../../../../shared/Interfaces";
+interface Props {
+  project: Project;
+}
+const StarButton: FC<Props> = ({ project }) => {
+  const { loading, isLoggedIn, user } = useSession();
+  const params = useParams();
+
+  const starProject = () => {
+    fetch(`/api/v1/projects/${project.id}/star`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: user, project: params.projectId }),
+    });
+    console.log(user);
+  };
+  return (
+    <ActionButton onClick={() => starProject()}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+        />
+      </svg>
+    </ActionButton>
+  );
 };
 
-export default function StarButton() {
-  return <ActionButton onClick={() => starProject()}>Star</ActionButton>;
-}
+export default StarButton;
--- frontend/src/components/project_page/pages/ProjectLandingPage/index.tsx
+++ frontend/src/components/project_page/pages/ProjectLandingPage/index.tsx
@@ -2,27 +2,17 @@ import { useOutletContext, useParams } from "react-router-dom";
 import { ProjectPageContext } from "../../layouts/ProjectPageLayout";
 import ProjectLanding from "./components/ProjectShowcase";
 import ApplyButtonContainer from "../../components/ApplyFlow/ApplyButtonContainer";
-import ActionButton from "../../../controls/ActionButton";
-import { useSession } from "../../../../hooks/session";
+
 import StarButton from "./components/StarButton";
 const ProjectLandingPage = () => {
   const { project, setProject } = useOutletContext<ProjectPageContext>();
-  const { loading, isLoggedIn, user } = useSession();
-  const params = useParams();
-  const starProject = () => {
-    fetch(`/api/v1/projects/${params.projectId}/star`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ user: user, project: params.projectId }),
-    });
-    console.log(user);
-  };
+  console.log(project);
   return (
     <>
-      <div>
-        <ActionButton onClick={() => starProject()}>Star</ActionButton>
+      <div className="flex m-1 p-1 justify-end">
+        <div className="basis-1/4">
+          <StarButton project={project}></StarButton>
+        </div>
       </div>
       <div className="flex flex-col-reverse md:flex-row">
         <main className="basis-3/4">
