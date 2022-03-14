6e3e4117b9f766e69390f7b785ebd60e0618c3e7
Matthew <8432061+mtso@users.noreply.github.com>
2022-03-13 22:23:18 -0700

    Add hero section to front page (#90)
    * Add Suggested Searches to FrontPage
    
    * Update DESIGN doc
    
    * Make logging more consistent
    
    * Add Hero illustration to FrontPage

--- frontend/src/components/front_page/FrontPage.tsx
+++ frontend/src/components/front_page/FrontPage.tsx
@@ -1,10 +1,18 @@
+import Hero from "./Hero";
 import TrendingSearches from "./TrendingSearches";
 
 const FrontPage = () => {
   return (
-    <div className="mx-3 my-4 md:mx-8 md:my-12">
-      <TrendingSearches />
-    </div>
+    <>
+      <div className="bg-white pt-4 md:pt-12">
+        <div className="mx-3 _my-4 md:mx-8 _md:my-12">
+          <Hero />
+        </div>
+      </div>
+      <div className="mx-3 my-4 md:mx-8 md:my-12">
+        <TrendingSearches />
+      </div>
+    </>
   );
 };
 
