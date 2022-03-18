a27ccc8fe6f4931815d8e63e2f4b617824a39ec4
seann1 <seanniesen@gmail.com>
2022-03-18 14:19:31 -0700

    reviewing another pr

--- frontend/src/components/projects/MyProjects.tsx
+++ frontend/src/components/projects/MyProjects.tsx
@@ -1,6 +1,15 @@
 import React, { FC, useEffect } from "react";
+import { useSession } from "../../hooks/session";
 
 const MyProjects: FC = () => {
+  const { loading, isLoggedIn, user } = useSession();
+  console.log(user);
+  useEffect(() => {
+    fetch(`api/v1/members?user=${user?.id}`).then(async (response) => {
+      if (response.status === 200) {
+      }
+    });
+  }, []);
   return <div>MyProjects</div>;
 };
 
