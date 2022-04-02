f7fff22d4110a3e6fee22719ddd4b765e56c7899
mtso <~@mtso.io>
2022-04-01 23:38:46 -0700

    Remove bubble if no unread

--- frontend/src/components/navbar/NotificationIcon.tsx
+++ frontend/src/components/navbar/NotificationIcon.tsx
@@ -8,7 +8,9 @@ const NotificationIcon = () => {
   return (
     <button className="relative inline-flex items-center mr-2 justify-center p-0.5 overflow-hidden text-sm font-medium text-emerald-200 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800">
       <span className="relative px-3 py-2.5 transition-all ease-in duration-75 bg-gray-700 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-        <div className="right-[0.7rem] top-[0.7rem] absolute w-2 h-2 rounded-full border-[1px] border-gray-700 bg-yellow-400"></div>
+        {hasUnread && (
+          <div className="right-[0.7rem] top-[0.7rem] absolute w-2 h-2 rounded-full border-[1px] border-gray-700 bg-yellow-400" />
+        )}
         <BellIcon className="inline w-4 h-4" />
       </span>
     </button>
